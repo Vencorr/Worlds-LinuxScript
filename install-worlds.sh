010237b949a8fc259458252bd7e2c8d767d4f531
Nicole George <vencorr@zoho.com>
2020-01-30 21:46:11 -0600

    Many things changed
    Perhaps this actually works properly now.

--- install-worlds.sh
+++ install-worlds.sh
@@ -10,46 +10,46 @@ start () {
 	if [ $1 == "fresh" ]; then
 		rm -rf $WINEPREFIX
 	fi
-	prefix
+	download
+}
+
+download () {
+ cd "$DIR/downloads"
+ wget https://github.com/Vencorr/Worlds-LinuxScript/blob/master/files.txt
+ wget -i files.txt
+ prefix
 }
 
 prefix () {
-	echo "Settings up the Wine prefix..."
-	wine init
+ echo "Installing components..."
 	if ! [ -x "$(command -v winetricks)" ]; then
 		echo "Error: 'winetricks' not found! Please add it to your path or install it via your package manager."
 		exit 1
 	fi
-	echo "Self updating winetricks..."
 	sudo winetricks --self-update
-	echo "Installing components..."
-	winetricks gdiplus ddr=gdi glsl=disabled allfonts l3codecx devenum quartz wmp10 ie8 win7
+	winetricks win7 corefonts droid ddr=gdi ie8 devenum wmp9 dmsynth wmv9vcm directplay quartz
 	install
 }
 
 install () {
 	cd "$DIR/downloads"
 	echo "Downloading Java 6u45 Windows i586..."
-	wget --user=$(zenity --forms --title="Oracle Login" --text="An Oracle account is required to download the installer" --add-entry="Email") --password=$(zenity --forms --title="Oracle Login" --text="An Oracle account is required to download the installer" --add-password="Password") https://download.oracle.com/otn/java/jdk/6u45-b06/jre-6u45-windows-i586.exe
-	if ! [ -f "$DIR/downloads/jre-6u45-windows-i586.exe" ]; then
-		echo "JRE6 installer not found! Aborting!"
-		exit 1
-	fi
-	wine jre-6u45-windows-i586 /s
-	echo "Downloading Installer..."
-	wget "http://cache.worlds.com/downloads/1900/Worlds1900.exe"
+	#wget --user=$(zenity --forms --title="Oracle Login" --text="An Oracle account is required to download the installer" --add-entry="Email") --password=$(zenity --forms --title="Oracle Login" --text="An Oracle account is required to download the installer" --add-password="Password") https://download.oracle.com/otn/java/jdk/6u45-b06/jre-6u45-windows-i586.exe
+	#if ! [ -f "$DIR/downloads/jre-6u45-windows-i586.exe" ]; then
+	#	echo "JRE6 installer not found! Aborting!"
+	#	exit 1
+	#fi
 	echo "Installing Worlds 1900. Please complete the setup."
-	wine "Worlds1900.exe"
+ wine jre-6u23-windows-i586-s /s
+	wine Worlds1900.exe /s
 	killall run.exe Worlds1900.exe javaw.exe
 	audio
 }
 
 audio () {
-	echo "Setting up Audio prerequisites..."
+	echo "Setting up Audio prerequisites. Please complete the setups."
 	cd "$DIR/downloads"
-	wget "https://www.dropbox.com/s/el0co8k0n0ps6a2/BCM1043.exe"
-	wine BCM1043.exe /s
-	wget "https://github.com/Nevcairiel/LAVFilters/releases/download/0.74.1/LAVFilters-0.74.1-Installer.exe"
+	wine K-Lite_Codec_Pack_1535_Full.exe /s
 	wine LAVFilters-0.74.1-Installer.exe /s
 	script
 }
