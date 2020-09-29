0d1cb5099429cd73b11c7769b1fed750e94be644
Nicholas George <vencorr@zoho.com>
2020-09-29 15:28:22 -0500

    Added 'mimeassoc=off' to default Winetricks
    Prevents K-Lite to handle system association

--- worlds-installer.sh
+++ worlds-installer.sh
@@ -9,7 +9,7 @@ export INSTALLER="$WORLDSDIR/WorldsInstaller.exe"
 
 export WINE=$(which wine)
 
-export WINETRICKSINSTALL="win7 corefonts droid ddr=gdi devenum wmp9 dmsynth wmv9vcm directplay quartz ie8 quartz"
+export WINETRICKSINSTALL="win7 corefonts droid ddr=gdi devenum wmp9 dmsynth wmv9vcm directplay quartz ie8 quartz mimeassoc=off"
 
 start () {
 	errorcheck
